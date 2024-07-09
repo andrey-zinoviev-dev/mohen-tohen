fc9a388e0cbc87b687ea9b0095faa99c01888634
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-07-09 04:09:50 +0300

    updated seller page

--- src/SellerPage.tsx
+++ src/SellerPage.tsx
@@ -1,15 +1,25 @@
 import { useLocation } from "react-router-dom"
 import { BrandsInterface } from "./interfaces";
+import "./SellerPage.css";
 import Goods from "./Goods";
+
 export default function SellerPage() {
   const location = useLocation();
   const state = location.state as BrandsInterface;
   console.log(state);
   return (
-    <section>
-      <img src={state.cover}></img>
-      <h3>{state.name}</h3>
-      <p>{state.description}</p>
+    <section className="seller">
+      <div className="seller__warpper">
+        <img className="seller__warpper-img" src={state.cover}></img>
+        <div className="seller__wrapper_details">
+          <h3>{state.name}</h3>
+          <p>{state.description}</p>
+          <div>
+          <span>5.0</span> <span>53 отзыва</span>
+          </div>
+          <button>Сделать персональный заказ</button>
+        </div>
+      </div>
       <Goods category="Что можно приобрести" goods={state.goodsCollection} />
     </section>
   )
