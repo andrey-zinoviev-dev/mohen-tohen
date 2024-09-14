767edfd7075395ce4f97628e9808717a8ab95a0e
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-09-14 04:47:55 +0300

    erased comments

--- src/Good.tsx
+++ src/Good.tsx
@@ -1,57 +1,15 @@
-import { useAppDispatch, useAppSelector } from "./hooks";
-// import { addRemoveToFavUser } from "./features/userSlice";
-import { changeMessage } from "./features/notificationSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpFromBracket, faCheck, faCirclePlus, faHeart, faShare, faShareNodes, faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUpFromBracket, faCirclePlus, } from "@fortawesome/free-solid-svg-icons";
 import { GoodComponentInterface } from "./interfaces";
-import { add, remove } from "./features/basketSlice";
-import { useNavigate } from "react-router-dom";
+// import { add, remove } from "./features/basketSlice";
+// import { useNavigate } from "react-router-dom";
 import LikeButton from "./LikeButton";
-import BasketButton from "./BasketButton";
-import { usePostGoodToFavouriteMutation } from "./features/apiSlice";
+// import BasketButton from "./BasketButton";
+// import { usePostGoodToFavouriteMutation } from "./features/apiSlice";
 export default function Good({good}:GoodComponentInterface) {
-
-
-    //RTK
-    // const {
-    //     data: addedToFavs = false as boolean
-    // } = usePostGoodToFavouriteMutation(good._id!)
-    
-    // const userStateFavs = useAppSelector((state) => {
-    //     return state.user.favourites;
-    // });
-    // const basketState = useAppSelector((state) => {
-    //     return state.basket.goods;
-    // })
-
-    const userState = useAppSelector((state) => {
-        return state.user;
-    })
-
-    //derived state
-    // const goodInFavourites = userState.favourites && userState.favourites.find((favGood) => {
-    //     return favGood.title === good.title;
-    // });
-    // const goodInBasket = userState.basket && userState.basket.find((basketGood) => {
-    //     return basketGood.title === good.title;
-    // });
-
-    //navigate
-    const navigate = useNavigate();
 
     return (
         <>
-            {/* <div className="goods__ul-li-text-wrapper">
-                
-                
-
-            </div> */}
-                            {/* <div className="goods__ul-li-heart">
-                  <p className="goods__ul-li-title">{good.title}</p>
-                  <LikeButton good={ good } />
-                </div> */}
-                {/* <span>{good.price}</span> */}
-                {/* {good.colors && <span>Цвета: {good.colors.length}</span>} */}
             <div className="goods__ul-li-actions-wrapper">
                     <img className="goods__ul-li-img" src={good.cover}></img>
                     <div className="goods__ul-li-buttons">
@@ -69,20 +27,7 @@ export default function Good({good}:GoodComponentInterface) {
                     <FontAwesomeIcon icon={faCirclePlus} />
                     Добавить в корзину
                 </button>
-                {/* <BasketButton good={good} /> */}
             </div>
-            {/* <button className="goods__ul-li-btn" onClick={(evt) => {
-                evt.stopPropagation();
-                {!goodInBasket ? 
-                    dispatch(add({...good, selectedColor: good.colors && good.colors[0]})) 
-                    :
-                    dispatch(remove(good)) 
-
-                }
-                dispatch(changeMessage({message: goodInBasket ? `Товар ${good.title} убран из корзины` : `Товар ${good.title} добавлен в корзину`}))
-            }}>
-                {goodInBasket ? <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> : <FontAwesomeIcon icon={faShoppingBag} />}
-            </button> */}
         </>
     )
 }
--- src/Goods.tsx
+++ src/Goods.tsx
@@ -1,72 +1,12 @@
-import { GoodInterface, GoodsInterface } from "./interfaces"
+import { GoodsInterface } from "./interfaces"
 import './Goods.css'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHeart } from "@fortawesome/free-solid-svg-icons"
-// import { faHeart } from "@fortawesome/free-regular-svg-icons"
-import { faShoppingBag } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate } from "react-router-dom"
-import { add } from "./features/basketSlice";
-// import { addToFavourite } from "./features/favouriteSlice"
-import { useAppDispatch, useAppSelector } from "./hooks"
-import { toggleFavourite } from "./features/goodsSlice"
-import { changeMessage } from "./features/notificationSlice"
-// import { addRemoveToFavUser } from "./features/userSlice"
 import Good from "./Good"
 
-export default function Goods({ goods, inAccountPage }:GoodsInterface) {
-  const dispatch = useAppDispatch();
+export default function Goods({ goods }:GoodsInterface) {
   const navigate = useNavigate();
 
-  //redux
-  const userStateFavs = useAppSelector((state) => {
-    return state.user.favourites;
-  })
-
-  //local state
-  // const [selectedGood, setSelectedGood] = React.useState<GoodInterface | null>(null);
-
   return (
-    // <section className={!inAccountPage ? "goods" : "goods goods_inaccount"}>
-    //   <h3>{category}</h3>
-    //   {goods && goods.length > 0 ? <ul className="goods__ul">
-    //     {goods && goods.map((good) => {
-    //       return <li key={good.title} className="goods__ul-li" onClick={() => {
-    //         navigate(`/goods/${good.title}`, {
-    //           state: good,
-    //           preventScrollReset: false,
-    //         })
-    //       }}>
-    //           <div className="goods__ul-li-text-wrapper">
-    //             <div className="goods__ul-li-heart">
-    //               <p className="goods__ul-li-title">{good.title}</p>
-    //               {!inAccountPage && <button onClick={(evt) => {
-    //                 evt.stopPropagation();
-    //                 // dispatch(addToFavourite());
-    //                 dispatch(toggleFavourite({...good, quantity: 1}));
-    //                 dispatch(changeMessage(`Товар ${good.title} добавлен в избранное`))
-    //               }}>
-    //                 <FontAwesomeIcon className="goods__ul-li-heart-svg" style={{color: good.favourite ? "#FF8261" : "#F7F7F7"}} icon={faHeart}/>
-    //               </button>}
-    //             </div>
-    //             <span>{good.price}</span>
-    //             {good.colors && <span>Цвета: {good.colors.length}</span>}
-
-    //           </div>
-    //           <img className="goods__ul-li-img" src={good.cover}></img>
-    //         {!inAccountPage && <button className="goods__ul-li-btn" onClick={(evt) => {
-    //           evt.stopPropagation();
-    //           dispatch(add({...good, quantity: 1, selectedColor: good.colors && good.colors[0]}));
-    //           dispatch(changeMessage(`Товар ${good.title} добавлен в корзину`))
-    //         }}>
-    //           <FontAwesomeIcon icon={faShoppingBag} />
-    //         </button>}
-    //       </li>
-    //     })}
-    //   </ul>
-    //   :
-    //   <p>Товаров в категории {category} пока нет, но их всегда можно добавить</p>
-    //   }
-    // </section>
     <>
     {goods && goods.length > 0 ? <ul className="goods__ul">
         {goods && goods.map((good) => {
@@ -77,35 +17,6 @@ export default function Goods({ goods, inAccountPage }:GoodsInterface) {
             })
           }}>
             <Good good={good}></Good>
-              {/* <div className="goods__ul-li-text-wrapper">
-                <div className="goods__ul-li-heart">
-                  <p className="goods__ul-li-title">{good.title}</p>
-                  <button onClick={(evt) => {
-                    evt.stopPropagation();
-                    dispatch(addRemoveToFavUser(good));
-                    // dispatch(addToFavourite());
-                    // dispatch(toggleFavourite({...good, quantity: 1}));
-                    dispatch(changeMessage(userStateFavs.find((favGood) => {
-                      return favGood.title === good.title;
-                    }) ? `Товар ${good.title} убран из избранных` : `Товар ${good.title} добавлен в избранное`))
-                  }}>
-                    <FontAwesomeIcon className="goods__ul-li-heart-svg" style={{color: userStateFavs.find((favGood) => {
-                      return favGood.title === good.title;
-                    }) ? "#FF8261" : "#F7F7F7"}} icon={faHeart}/>
-                  </button>
-                </div>
-                <span>{good.price}</span>
-                {good.colors && <span>Цвета: {good.colors.length}</span>}
-
-              </div>
-              <img className="goods__ul-li-img" src={good.cover}></img>
-            {!inAccountPage && <button className="goods__ul-li-btn" onClick={(evt) => {
-              evt.stopPropagation();
-              dispatch(add({...good, quantity: 1, selectedColor: good.colors && good.colors[0]}));
-              dispatch(changeMessage(`Товар ${good.title} добавлен в корзину`))
-            }}>
-              <FontAwesomeIcon icon={faShoppingBag} />
-            </button>} */}
           </li>
         })}
       </ul>
