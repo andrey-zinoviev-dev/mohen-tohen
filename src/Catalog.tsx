0c08176132a86a01e94a843f8064278ba2bb876f
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-11-12 18:18:41 +0300

    finalized filter

--- src/Catalog.tsx
+++ src/Catalog.tsx
@@ -9,12 +9,49 @@ import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import PortalComp from "./PortalComp";
 import PortalCentered from "./PortalCentered";
+import { useSearchParams } from "react-router-dom";
 // import { useSearchParams } from "react-router-dom";
 // import { faCheck } from "@fortawesome/free-solid-svg-icons";
 export default function Catalog() {
     const {
         data: goods = [] as GoodInterface[],
     } = useGetGoodsQuery();
+
+    //search params
+    const [searchParams] = useSearchParams();
+
+    const urlOjb = (Object.fromEntries([...searchParams]));
+    // console.log(urlOjb);
+    //url conversion
+    const urlConverted = Object.fromEntries(Object.entries(urlOjb).filter((entry) => {
+        // console.log(entry);
+        return entry[1].length > 0;
+    }).map(([key, value]) => {
+        if(key === "categories") {
+            return [key, value === "" ? [] : value.split(",")];
+        }
+        if(key === "stock") {
+            return [key, value === "false" ? false : true];
+            // console.log(Boolean(value));
+        }
+        if(key === 'colors') {
+            return [key, value === "" ? [] : value.split(",")]
+            // console.log(value.split(","));
+        }
+        if(key === 'minPrice') {
+           return [key, +value];
+        }
+        if(key=== "maxPrice") {
+            return [key, +value];
+        }
+        return [key, value.split(",")]
+    })) as {
+        categories: string[],
+        stock?: boolean,
+        minPrice: number,
+        maxPrice: number,
+        colors: string[]
+    };
 
     //sellers
     const sellersArray = goods.map((good) => {
@@ -28,20 +65,64 @@ export default function Catalog() {
     //state
     const [openedFilter, setOpenedFilter] = useState<boolean>(false);
     const [catalogGoods, setCatalogGoods] = useState<GoodInterface[]>(goods);
-    // //search params
-    // const [searchParams] = useSearchParams();
-    // const paramsType = searchParams.get("type");
-    // console.log(paramsType);
 
     //functions
     function applyFilters(goods:GoodInterface[]) {
-        // setOpenedFilter(false);
         setCatalogGoods(goods);
     }
 
+    // useEffect(() => {
+    //     console.log()
+    // }, [])
+
     useEffect(() => {
-        goods.length > 0 && setCatalogGoods(goods);
-    }, [goods])
+        if(goods.length > 0)  {
+            // console.log(urlConverted)
+            if(Object.entries(urlConverted).length > 0) {
+
+                let resultArray = goods;
+
+                if(urlConverted.categories && urlConverted.categories.length > 0) {
+                    resultArray = resultArray.filter((good) => {
+                        return urlConverted.categories && urlConverted.categories.find((category) => {
+                            return category === good.category;
+                        });
+                    })
+                }
+                
+                if(urlConverted.stock) {
+                    resultArray = resultArray.filter((good) => {
+                        return urlConverted.stock ? good.batch > 0 : good.batch === 0;
+                    })
+                }
+
+                if(urlConverted.minPrice >= 3000) {
+                    resultArray = resultArray.filter((good) => {
+                        return good.price >= urlConverted.minPrice;
+                    })
+                }
+
+                if(urlConverted.maxPrice <= 100000) {
+                    // console.log('update goods max price here')
+                    resultArray = resultArray.filter((good) => {
+                        return good.price <= urlConverted.maxPrice;
+                    })
+                }
+
+                if(urlConverted.colors && urlConverted.colors.length > 0) {
+                    resultArray = resultArray.filter((good) => {
+                        return urlConverted.colors && urlConverted.colors.find((color) => {
+                            return color === good.color;
+                        })
+                    })
+                }
+                setCatalogGoods(resultArray);
+            } else {
+                setCatalogGoods(goods);
+            }
+        }
+
+    }, [goods]);
 
     return (
         <section className="category">
@@ -61,7 +142,7 @@ export default function Catalog() {
                     <FontAwesomeIcon icon={faXmark} />
                 </button>
                 <PortalCentered>
-                    <Filter goods={goods} sellers={sellersWithNoRepeats} applyFilters={applyFilters}></Filter>
+                    <Filter urlConverted={urlConverted} goods={goods} sellers={sellersWithNoRepeats} closeFilter={setOpenedFilter} applyFilters={applyFilters}></Filter>
                 </PortalCentered>
             </PortalComp>, document.body)}
         </section>
--- src/Filter.tsx
+++ src/Filter.tsx
@@ -4,29 +4,18 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { categories } from "./utils";
 import { createSearchParams, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { GoodInterface } from "./interfaces";
 // import { UserInterface } from "./features/userSlice";
 
-export default function Filter({sellers, goods, applyFilters}: {sellers: (string | undefined)[], goods: GoodInterface[], applyFilters: (good: GoodInterface[]) => {}}) {
-    const [searchParams] = useSearchParams();
-    const urlOjb = (Object.fromEntries([...searchParams]));
-    // console.log(urlOjb);
-    //url conversion
-    const urlConverted = Object.fromEntries(Object.entries(urlOjb).map(([key, value]) => {
-        if(key === "categories") {
-            return [key, value === "" ? [] : value.split(",")];
-        }
-        if(key === "stock") {
-            return [key, Boolean(value)];
-            // console.log(Boolean(value));
-        }
-        if(key === 'colors') {
-            return [key, value === "" ? [] : value.split(",")]
-            // console.log(value.split(","));
-        }
-        return [key, value.split(",")]
-    }));
+export default function Filter({urlConverted, sellers, goods, applyFilters, closeFilter}: {urlConverted: {
+    categories: string[],
+    stock?: boolean,
+    minPrice: number,
+    maxPrice: number,
+    colors: string[]
+    },
+    sellers: (string | undefined)[], goods: GoodInterface[], closeFilter: React.Dispatch<React.SetStateAction<boolean>>, applyFilters: (good: GoodInterface[]) => void}) {
 
     //navigate
     const navigate = useNavigate();
@@ -59,10 +48,9 @@ export default function Filter({sellers, goods, applyFilters}: {sellers: (string
 
     useEffect(() => {
         let resultArray = goods;
-        // console.log(resultArray);
-        if(filterState.categories.length > 0) {
-            // console.log(filterState.categories);
-            // console.log("filter array by categories")
+      
+        if(filterState.categories && filterState.categories.length > 0) {
+
             resultArray = resultArray.filter((good) => {
                 return filterState.categories && filterState.categories.find((category) => {
                     return category === good.category;
@@ -77,75 +65,24 @@ export default function Filter({sellers, goods, applyFilters}: {sellers: (string
         }
 
         if(filterState.minPrice >= 3000) {
-            // console.log('update goods min price here')
             resultArray = resultArray.filter((good) => {
                 return good.price >= filterState.minPrice;
             })
-            // console.log(resultArray);
         }
 
         if(filterState.maxPrice <= 100000) {
-            // console.log('update goods max price here')
             resultArray = resultArray.filter((good) => {
                 return good.price <= filterState.maxPrice;
             })
         }
 
-        if(filterState.colors.length > 0) {
+        if(filterState.colors && filterState.colors.length > 0) {
             resultArray = resultArray.filter((good) => {
                 return filterState.colors && filterState.colors.find((color) => {
                     return color === good.color;
                 })
             })
         }
-        // console.log(resultArray);
-        applyFilters(resultArray);
-    }, [])
-
-    useEffect(() => {
-        // console.log(goods);
-        // console.log(filterState);
-        let resultArray = goods;
-        // console.log(resultArray);
-        if(filterState.categories.length > 0) {
-            // console.log(filterState.categories);
-            // console.log("filter array by categories")
-            resultArray = resultArray.filter((good) => {
-                return filterState.categories && filterState.categories.find((category) => {
-                    return category === good.category;
-                });
-            })
-        }
-        
-        if(filterState.stock) {
-            resultArray = resultArray.filter((good) => {
-                return filterState.stock ? good.batch > 0 : good.batch === 0;
-            })
-        }
-
-        if(filterState.minPrice >= 3000) {
-            // console.log('update goods min price here')
-            resultArray = resultArray.filter((good) => {
-                return good.price >= filterState.minPrice;
-            })
-            // console.log(resultArray);
-        }
-
-        if(filterState.maxPrice <= 100000) {
-            // console.log('update goods max price here')
-            resultArray = resultArray.filter((good) => {
-                return good.price <= filterState.maxPrice;
-            })
-        }
-
-        if(filterState.colors.length > 0) {
-            resultArray = resultArray.filter((good) => {
-                return filterState.colors && filterState.colors.find((color) => {
-                    return color === good.color;
-                })
-            })
-        }
-        // console.log(resultArray);
         setFilteredArray(resultArray);
     }, [filterState]);
 
@@ -153,123 +90,123 @@ export default function Filter({sellers, goods, applyFilters}: {sellers: (string
         <div>
             <h3>Фильтры</h3>
             <div>
-            <ul>
-                <li>
-                    <FilterItem text="Категория">
-                        <div>
-                            {categories.map((category) => {
-                                return <label key={category.title}>
-                                    <input checked={filterState.categories?.includes(category.title) && true} key={category.title} onChange={(evt) => {
-                                        setFilterState((prevValue) => {
-                                            return {...prevValue, categories: prevValue.categories ? prevValue.categories.includes(evt.target.value) ? prevValue.categories.filter((category) => {
-                                                return category !== evt.target.value
-                                            }) : prevValue.categories && [...prevValue.categories, evt.target.value]
-                                            :
-                                            [category.title]
-                                            }
-                                        })
-                                        // console.log(evt.target.value)
-                                    }} type="checkbox" value={category.title}></input>
-                                    {category.title}
+                <ul>
+                    <li>
+                        <FilterItem text="Категория">
+                            <div>
+                                {categories.map((category) => {
+                                    return <label key={category.title}>
+                                        <input checked={filterState.categories?.includes(category.title) && true} key={category.title} onChange={(evt) => {
+                                            setFilterState((prevValue) => {
+                                                return {...prevValue, categories: prevValue.categories ? prevValue.categories.includes(evt.target.value) ? prevValue.categories.filter((category) => {
+                                                    return category !== evt.target.value
+                                                }) : prevValue.categories && [...prevValue.categories, evt.target.value]
+                                                :
+                                                [category.title]
+                                                }
+                                            })
+                                            // console.log(evt.target.value)
+                                        }} type="checkbox" value={category.title}></input>
+                                        {category.title}
+                                    </label>
+                                })}
+                            </div>
+                        </FilterItem>
+                        {/* <button>Категория</button> */}
+                    </li>
+                    <li>
+                        <FilterItem text="Наличие">
+                            <label>
+                                Наличие
+                                <input checked={filterState.stock ? true : false} name="stock" onChange={() => {
+                                    setFilterState((prevValue) => {
+                                        return {...prevValue, stock: !prevValue.stock}
+                                    })
+                                }} type="checkbox" value={"yes"}>
+                                </input>
+                            </label>
+                        </FilterItem>
+                        {/* <button>Наличие</button> */}
+                    </li>
+                    <li>
+                        <FilterItem text="Цена">
+                            <input  onChange={(evt) => {
+                                setFilterState((prevValue) => {
+                                    return {...prevValue, minPrice: +evt.target.value}
+                                })
+                            }} placeholder="от 3000"></input>
+                            <input onChange={(evt) => {
+                                setFilterState((prevValue) => {
+                                    return {...prevValue, maxPrice: +evt.target.value}
+                                })
+                            }} placeholder="до 100.000"></input>
+                        </FilterItem>
+                        {/* <button>Цена</button>
+                    </li>
+                    <li>
+                        <FilterItem text="Размеры">
+                            <label>
+                                Размеры
+                                <input placeholder="от"></input>
+                                <input placeholder="до"></input>
+                            </label>
+                        </FilterItem>
+                        {/* <button>Размеры</button> */}
+                    </li>
+                    <li>
+                        <FilterItem text="Цвета">
+                            {colors.map((color) => {
+                                return <button key={color} onClick={() => {
+                                    setFilterState((prevValue) => {
+                                        return {...prevValue, colors: prevValue.colors ? prevValue.colors.includes(color) ? prevValue.colors.filter((prevColor) => {
+                                            return prevColor !== color
+                                        }) : [...prevValue.colors, color]
+
+                                        :
+
+                                        [color]
+                                        
+                                        }
+                                    })
+                                }} style={{backgroundColor: color}}></button>
+                            })}
+                        </FilterItem>
+                        
+                    </li>
+                    {/* <li>
+                        <FilterItem text="Материалы" />
+                        <button>Материалы</button>
+                    </li> */}
+                    <li>
+                        <FilterItem text="Дизайнеры">
+                            {sellers.map((seller) => {
+                                return <label>
+                                    {seller && seller}
+                                    <input type="checkbox" value={seller}></input>
                                 </label>
                             })}
-                        </div>
-                    </FilterItem>
-                    {/* <button>Категория</button> */}
-                </li>
-                <li>
-                    <FilterItem text="Наличие">
-                        <label>
-                            Наличие
-                            <input checked={filterState.stock ? true : false} name="stock" onChange={() => {
-                                setFilterState((prevValue) => {
-                                    return {...prevValue, stock: !prevValue.stock}
-                                })
-                            }} type="checkbox" value={"yes"}>
-                            </input>
-                        </label>
-                    </FilterItem>
-                    {/* <button>Наличие</button> */}
-                </li>
-                <li>
-                    <FilterItem text="Цена">
-                        <input value={filterState.minPrice} onChange={(evt) => {
-                            setFilterState((prevValue) => {
-                                return {...prevValue, minPrice: +evt.target.value}
-                            })
-                        }} placeholder="от 3000"></input>
-                        <input value={filterState.maxPrice} onChange={(evt) => {
-                            setFilterState((prevValue) => {
-                                return {...prevValue, maxPrice: +evt.target.value}
-                            })
-                        }} placeholder="до 100.000"></input>
-                    </FilterItem>
-                    {/* <button>Цена</button>
-                </li>
-                <li>
-                    <FilterItem text="Размеры">
-                        <label>
-                            Размеры
-                            <input placeholder="от"></input>
-                            <input placeholder="до"></input>
-                        </label>
-                    </FilterItem>
-                    {/* <button>Размеры</button> */}
-                </li>
-                <li>
-                    <FilterItem text="Цвета">
-                        {colors.map((color) => {
-                            return <button key={color} onClick={() => {
-                                setFilterState((prevValue) => {
-                                    return {...prevValue, colors: prevValue.colors ? prevValue.colors.includes(color) ? prevValue.colors.filter((prevColor) => {
-                                        return prevColor !== color
-                                    }) : [...prevValue.colors, color]
-
-                                    :
-
-                                    [color]
-                                    
-                                    }
-                                })
-                            }} style={{backgroundColor: color}}></button>
-                        })}
-                    </FilterItem>
-                    
-                </li>
-                {/* <li>
-                    <FilterItem text="Материалы" />
-                    <button>Материалы</button>
-                </li> */}
-                <li>
-                    <FilterItem text="Дизайнеры">
-                        {sellers.map((seller) => {
-                            return <label>
-                                {seller && seller}
-                                <input type="checkbox" value={seller}></input>
-                            </label>
-                        })}
-                    </FilterItem>
-                    {/* <button>
-                        Дизайнер
-                    </button> */}
-                </li>
-            </ul>
+                        </FilterItem>
+                        {/* <button>
+                            Дизайнер
+                        </button> */}
+                    </li>
+                </ul>
             </div>
             
             <button onClick={() => {
-                // console.log(filteredArray);
                 const result = Object.entries(filterState).map(([key, val]) => {
-                    // console.log(val);
                     return [key, val.toString()]
                 });
 
                 navigate({
                     pathname: `../catalog`,
                     search: `${createSearchParams(Object.fromEntries(result))}`
-                })
+                });
+                closeFilter(false)
                 applyFilters(filteredArray);
             }}>
                 Показать ({filteredArray.length})
+                {/* Показать ({filteredArray.length}) */}
                 {/* <FontAwesomeIcon icon={faCheck} /> */}
             </button>
             {/* <ul></ul> */}
