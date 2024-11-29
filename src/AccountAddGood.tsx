008e79d12159174dfd1220955c4c885798882216
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-11-29 20:26:49 +0300

    mobile layout changes

--- src/AccountAddGood.tsx
+++ src/AccountAddGood.tsx
@@ -136,8 +136,8 @@ export default function AccountAddGood() {
           <div className="addGoodform__text-wrapper-div">
             <label className="addGoodform__label">
               Описание
-              <textarea></textarea>
-              {/* <InputEl updateState={setFormData} placeHolder="Этот торшер сделан из премиальных материалов" name="description"></InputEl> */}
+              {/* <textarea></textarea> */}
+              <InputEl updateState={setFormData} placeHolder="Этот торшер сделан из премиальных материалов" name="description"></InputEl>
               {/* <input></input> */}
             </label>
           </div>
